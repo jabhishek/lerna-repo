0271b8fd6ce51349b0aa22b5eb8ab448f14c2a14
Abhishek Jain <abhi2000@gmail.com>
2019-05-21 18:56:19 +0100

    ...

--- packages/use-form/lib/index.js
+++ packages/use-form/lib/index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.sum = (numbers) => numbers.reduce((acc, num) => acc + num, 0);
+exports.sum = (numbers) => numbers.reduce((acc, num) => acc + num, 1);
 //# sourceMappingURL=index.js.map
